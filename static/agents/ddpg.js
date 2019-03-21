1f44073ba62ff85c8564d9c2e522240b75b35cfa
Slobodan Blazeski <slobodan.blazeski@gmail.com>
2019-03-21 21:33:50 +0100

    Fix DDPG

--- static/agents/ddpg.js
+++ static/agents/ddpg.js
@@ -51,28 +51,40 @@ const DDPG = (function () {
       }
     }
     sample() {
-      const experiences = sample(self.memory, this.batchSize);
-      return {
-        states: experiences.map(x => s.state), 
-        actions: experiences.map(x => s.action), 
-        rewards: experiences.map(x => s.reward), 
-        nextStates: experiences.map(x => s.nextState), 
-        dones: experiences.map(x => s.done),
-      };
+      const experiences = sample(this.memory, this.batchSize);
+      return tf.tidy(() => {
+        return {
+          states: tf.tensor(experiences.map(x => x.state)), 
+          actions: tf.tensor(experiences.map(x => x.action)), 
+          rewards: tf.tensor(experiences.map(x => [x.reward])), 
+          nextStates: tf.tensor(experiences.map(x => x.nextState)), 
+          dones: tf.tensor(experiences.map(x => [x.done])),
+        };
+      });
+    }
+
+    get length() {
+      return this.memory.length;
     }
   }
 
-  
-
   const hardUpdate = (source, target) => {
-    N.til(MIN_BUFFER_SIZE).forEach(i => 
-      target.weights[i].assign(source.weights[i]));
+    tf.tidy(() => {
+      target.layers.forEach((layer, i) => 
+      layer.setWeights(source.layers[i].getWeights()));
+    });
   }
 
   const softUpdate = (source, target, tau) => {
-    N.til(MIN_BUFFER_SIZE).forEach(i => 
-      target.weights[i].assign(source.weights[i].mul(tau)
-        + target.weights[i].mul(1-tau)));
+    tf.tidy(() => {
+      target.layers.forEach((layer, i) =>  {
+        const sWeights = source.layers[i].getWeights();
+        const tWeights = layer.getWeights();
+        const res = sWeights.map((s, i) => 
+          tf.mul(s, tau).add(tf.mul(tWeights[i], 1-tau)));
+        layer.setWeights(res);
+      });
+    });
   }
 
   const makeActor = (inputShape, outputShape, l1=96, l2=96) => tf.sequential({
@@ -83,16 +95,14 @@ const DDPG = (function () {
     ]
   });
 
-  const makeCritic = (inputShape, actionShape, l1=96, l2=96) => {
-    const stateInput = tf.input({shape: [inputShape]});
-    const actionInput = tf.input({shape: [actionShape]});
-    const fc1 = tf.layers.dense({units: l1, activation: 'relu'}).apply(stateInput);
-    const concat = tf.layers.concatenate().apply([fc1, actionInput]);
-    const fc2 = tf.layers.dense({units: l2, activation: 'relu'}).apply(concat);
-    const output = tf.layers.dense({units: 1}).apply(fc2);
-    const model = tf.LayersModel({inputs: [stateInput, actionInput], outputs: output});
-    return model;
-  }
+  const makeCritic = (inputShape, actionShape, l1=96, l2=96) => tf.sequential({
+    layers: [
+      tf.layers.dense({inputShape: [inputShape+actionShape], 
+        units: l1, activation: 'relu'}),
+      tf.layers.dense({units: l2, activation: 'relu'}),
+      tf.layers.dense({units: 1, activation: 'tanh'}),
+    ]
+  });
 
   class DDPG {
     constructor(stateSize, actionSize, fc1Units=96, fc2Units=96,
@@ -101,7 +111,7 @@ const DDPG = (function () {
       this.actionSize = actionSize;
       this.epsilon = epsilon;
       this.weightDecay = weightDecay;
-      this.noise = OUNoise(actionSize);
+      this.noise = new OUNoise(actionSize);
       this.actor = makeActor(stateSize, actionSize, fc1Units=fc1Units,
         fc2Units=fc2Units);
       this.actorTarget = makeActor(stateSize, actionSize, fc1Units=fc1Units,
@@ -112,12 +122,15 @@ const DDPG = (function () {
         fc2Units=fc2Units);
       this.actorOptimizer = tf.train.adam(lrActor);
       this.criticOptimizer = tf.train.adam(lrCritic);
-      hardUpdate(actor, actorTarget);
-      hardUpdate(critic, criticTarget);      
+      hardUpdate(this.actor, this.actorTarget);
+      hardUpdate(this.critic, this.criticTarget);      
     }
 
-    act (state, adNoise=true) {
-      return adNoise ? actor(state).add(this.noise.sample) : actor(state);
+    act (state, addNoise=true) {
+      return tf.tidy(() => {
+        const action = tf.squeeze(this.actor.predict(tf.tensor([state])));
+        return addNoise ? action.add(this.noise.sample()) : action;
+      });
     }
 
     reset () {
@@ -126,23 +139,27 @@ const DDPG = (function () {
 
     learn(experiences, gamma, tau=1e-3, epsilonDecay=1e-6) {
       const {states, actions, rewards, nextStates, dones} = experiences;
-      // Get predicted next-state actions and Q values from target models
-      const actionsNext = this.actorTarget(nextStates)
-      const qTargetsNext = self.criticTarget(nextStates, actionsNext);
-      // Compute Q targets for current states (y-i)
-      const qTargets = tf.add(rewards, tf.mul(tf.mul(gamma, qTargetsNext), tf.sub(1, dones)));
-      const qExpected = this.critic(states, actions);
-      // Critic update
-      this.criticOptimizer.minimize(() => {
-        const criticLoss = tf.losses.meanSquaredError(qExpected, qTargets);
-        // torch.nn.utils.clip_grad_norm_(self.critic.parameters(), 1)
-        return criticLoss;
-      });
-      // Actor update
-      this.actorOptimizer.minimize(() => {
-        actionsPred = this.actor(states);
-        actorLoss = -this.critic(states, actionsPred).mean();
-        return actorLoss;
+      tf.tidy(() => {
+        // Get predicted next-state actions and Q values from target models
+        const actionsNext = this.actorTarget.predict(nextStates)
+        const qTargetsNext = this.criticTarget.predict(tf.concat([nextStates, actionsNext], 1));
+        
+        // Critic update
+        this.criticOptimizer.minimize(() => {
+          // Compute Q targets for current states (y-i)
+          const qTargets = tf.add(rewards, tf.mul(tf.mul(gamma, qTargetsNext), tf.sub(1, dones)));
+          const qExpected = this.critic.predict(tf.concat([states, actions], 1));
+          const criticLoss = tf.losses.meanSquaredError(qExpected, qTargets);
+          // torch.nn.utils.clip_grad_norm_(self.critic.parameters(), 1)
+          return criticLoss;
+        });
+        // Actor update
+        this.actorOptimizer.minimize(() => {
+          const actionsPred = this.actor.predict(states);
+          //const actorLoss = -this.critic(states, actionsPred).mean();
+          const actorLoss = this.critic.predict(tf.concat([states, actionsPred], 1)).mean().mul(-1.);
+          return actorLoss;
+        });
       });
       // Targets update
       softUpdate(this.critic, this.criticTarget, tau);
@@ -153,9 +170,11 @@ const DDPG = (function () {
     }
   }
   
-  const MIN_BUFFER_SIZE = 1e5;
+  const MIN_BUFFER_SIZE = 1e3;
   const UPDATE_EVERY = 10;
   const GAMMA = 0.99;
+  const BUFFER_SIZE = 1e6;
+  const BATCH_SIZE = 32;
 
   const learn = (agent, buffer, stepNo) => {
     if (buffer.length > MIN_BUFFER_SIZE && stepNo % UPDATE_EVERY == 0) {
@@ -169,23 +188,27 @@ const DDPG = (function () {
     const {instanceId, observationSpace, actionSpace, maxSteps} = env;
     const inputSize = observationSpace.shape[0];
     const outputSize = actionSpace.shape[0];
-    const buffer = new ReplayBuffer()
+    const buffer = new ReplayBuffer(BUFFER_SIZE, BATCH_SIZE);
     const agent = new DDPG(inputSize, outputSize);
+    let maxReward = -Infinity;
     for (const epNo of N.til(maxEpisodes)) {
       let epReward = 0, observation, reward, done;
       observation = await API.environmentReset(instanceId);
       for (const stepNo of N.til(maxSteps)) {
         let action = await agent.act(observation).data();
+        action = Array.from(action);
         const prevState = observation;
         ({observation, reward, done} =
           await Util.stepResponse(instanceId, action, render));
-        
-        buffer.add(state, action, reward, nextState, done);
-        learn(agent, buffers, epNo, stepNo);
-        epReward += reward; 
+        reward = Math.max(reward, -1);
+        done = done ? 1. : 0.;
+        buffer.add(prevState, action, reward, observation, done);
+        learn(agent, buffer, epNo, stepNo);
+        epReward += reward;
+        //console.log(`EpNo ${epNo} stepNo ${stepNo} reward ${reward}`); 
         if (done) break;
       }
-      
+      console.log(`EpNo ${epNo}  epReward ${epReward}`); 
       if (epReward > maxReward) {
         maxReward =  epReward;
       }
@@ -194,10 +217,18 @@ const DDPG = (function () {
   };
 
   return {
-    continuous, 
+    continuous,
+    __internal__: {
+      DDPG,
+      ReplayBuffer,
+      hardUpdate,
+      softUpdate,
+      makeActor,
+      makeCritic,
+    },
   };
 })();
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-  module.exports = OUNoise;
+  module.exports = DDPG;
 }
--- static/index.js
+++ static/index.js
@@ -3,15 +3,18 @@ const start  = async () => {
   if (!Util.socket.connected) {
     return setTimeout(start, 100);
   }
-  console.log('RandomPlay.discrete');
-  await Util.run('CartPole-v0', RandomPlay.discrete, 100, false);
-  console.log('RandomSearch.discrete');
-  await Util.run('CartPole-v0', RandomSearch.discrete, 100, false);
-  console.log('HillClimbing.discrete');
-  await Util.run('CartPole-v0', HillClimbing.discrete, 100, false);
+  // console.log('RandomPlay.discrete');
+  // await Util.run('CartPole-v0', RandomPlay.discrete, 100, false);
+  // console.log('RandomSearch.discrete');
+  // await Util.run('CartPole-v0', RandomSearch.discrete, 100, false);
+  // console.log('HillClimbing.discrete');
+  // await Util.run('CartPole-v0', HillClimbing.discrete, 100, false);
 
-  console.log('ARS.continuous');
-  await Util.run('BipedalWalker-v2', ARS.continuous, 3200, false)
+  // console.log('ARS.continuous');
+  // await Util.run('BipedalWalker-v2', ARS.continuous, 3200, false);
+
+  console.log('DDPG');
+  await Util.run('BipedalWalker-v2', DDPG.continuous, 3200, false);
 }
 
 start();
